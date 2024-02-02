678e7c6384e22f81b4406354de528973455e65f2
Jérôme Bucquet <jerry.bucquet@gmail.com>
2024-02-02 23:50:05 +0100

    feat: add click event handler to cklickables

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,34 @@
-import { generateBackgroundColor } from '../modules';
+import { generateBackgroundColor, colorSVG, colorReflection } from '../modules';
+
+type ClickableElement = HTMLElement & { id: string };
 
 ((): void => {
-  generateBackgroundColor();
-  console.log('Hello, world!');
+  document.addEventListener('click', (evt): void => {
+    if (evt.target === null) return;
+
+    const color = generateBackgroundColor();
+    const target = evt.target as ClickableElement;
+
+    if (!target.matches('.clickable')) return;
+
+    const box = document.getElementById('box');
+    const path = document.querySelector('path');
+    const reflexOne = document.getElementById('reflex-one');
+    const reflexTwo = document.getElementById('reflex-two');
+
+    switch (target.id) {
+      case 'box':
+      case 'path':
+        colorSVG(box, path, color);
+        break;
+
+      case 'reflex-one':
+      case 'reflex-two':
+        colorReflection(reflexOne, reflexTwo, color);
+        break;
+
+      default:
+        target.style.backgroundColor = color;
+    }
+  });
 })();
