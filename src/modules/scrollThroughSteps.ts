ddc2050e3757c14dd204647bc2bb3c720e70df57
Jérôme Bucquet <jerry.bucquet@gmail.com>
2024-02-06 23:31:41 +0100

    feat: remove steps after scroll

--- src/modules/scrollThroughSteps.ts
+++ src/modules/scrollThroughSteps.ts
@@ -2,9 +2,18 @@ import { applyColorFromStep } from './applyColorFromStep';
 import { steps } from './handleClickableElement';
 
 let scrollIndex = 0;
+let removeStepsByIndexTimeout: NodeJS.Timeout;
 
 export const scrollThroughSteps = (mousePositionX: number): void => {
   if (steps.length === 0) return;
   scrollIndex = Math.floor((mousePositionX / window.innerWidth) * steps.length);
   applyColorFromStep(steps[scrollIndex]);
+};
+
+export const removeStepsByIndex = (): void => {
+  clearTimeout(removeStepsByIndexTimeout);
+  removeStepsByIndexTimeout = setTimeout(() => {
+    steps.slice(0, steps.length - scrollIndex);
+    console.log({ scrollIndex });
+  }, 2500);
 };
